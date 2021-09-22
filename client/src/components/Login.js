2430b2e122a488eb3535511031354fa07bb599b1
Igor <sergienko339@gmail.com>
2021-09-22 22:14:06 +0300

    added login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <input type="text" />
+      <input type="text" />
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Login;
